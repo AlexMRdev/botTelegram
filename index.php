5ec7c96724ab166a9f358ef4b8e126359c46687e
AlexMRdev <alexmrdev@gmail.com>
2022-02-14 20:54:16 +0100

    2 dia

--- index.php
+++ index.php
@@ -59,10 +59,12 @@ function categorias($chatId){
     
     for($i=0; $i<=9; $i++ ){
         $titulos=$array['channel']['item'][$i]['category'];
-        $titulos_simple=array_unique($titulos);
-        sendMessage($chatId,$titulos_simple);
-        // sendMessage($chatId,$titulos);
-    };    
+        // $titulos_simple=array_values(array_unique($titulos));
+        // sendMessage($chatId,$titulos_simple);
+        sendMessage($chatId,$titulos);
+    };
+        $titulos_simple=array_values(array_unique($titulos));
+        sendMessage($chatId,$titulos_simple);    
 };
 
 
