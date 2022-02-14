90182a978524610864bca50bf8962d54548a00f8
AlexMRdev <alexmrdev@gmail.com>
2022-02-14 20:56:45 +0100

    2 dia

--- index.php
+++ index.php
@@ -63,7 +63,7 @@ function categorias($chatId){
         // sendMessage($chatId,$titulos_simple);
         // sendMessage($chatId,$titulos);
     };
-        $titulos_simple=array_values(array_unique($titulos));
+        $titulos_simple=array_unique($titulos);
         sendMessage($chatId,$titulos_simple);    
 };
 
