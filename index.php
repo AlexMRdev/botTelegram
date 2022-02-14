bdefa1ae99b313238ae2c5e425b2217a31c9821e
AlexMRdev <alexmrdev@gmail.com>
2022-02-14 20:16:36 +0100

    2 dia

--- index.php
+++ index.php
@@ -34,23 +34,9 @@ function sendMessage($chatId, $response) {
     file_get_contents($url);
 };
 
-// function getPc($chatId){
-//     $context= stream_context_create(array('http'=> array('header'=>'Accept:application/xml')));
-//     $url='https://www.europapress.es/rss/rss.aspx';
-//     $xmlstring= file_get_contents($url, false, $context);
-//     $xml =simplexml_load_string($xmlstring, "SimpleXMLElement", LIBXML_NOCDATA);
-//     $json= json_encode($xml);
-//     $array= json_decode($json , TRUE);
-    
-//     for($i=0; $i<=9; $i++ ){
-//         $titulos=$array['channel']['item'][$i]['title']."<a href='".$array['channel']['item'][$i]['link']."'>+info</a>"; 
-//         sendMessage($chatId,$titulos);
-//     };    
-// };
-
 function getPc($chatId){
     $context= stream_context_create(array('http'=> array('header'=>'Accept:application/xml')));
-    $url='https://www.inoutviajes.com/rss/seccion/217/';
+    $url='https://www.europapress.es/rss/rss.aspx';
     $xmlstring= file_get_contents($url, false, $context);
     $xml =simplexml_load_string($xmlstring, "SimpleXMLElement", LIBXML_NOCDATA);
     $json= json_encode($xml);
@@ -59,19 +45,21 @@ function getPc($chatId){
     for($i=0; $i<=9; $i++ ){
         $titulos=$array['channel']['item'][$i]['title']."<a href='".$array['channel']['item'][$i]['link']."'>+info</a>"; 
         sendMessage($chatId,$titulos);
-    };
+    };    
 };
 
-// function buscarNoticia($chatId,$palabra){
-//     $context= stream_context_create(array('http'=> array('header'=>'Accept:application/xml')));
-//     $url='https://www.europapress.es/rss/rss.aspx';
-//     $xmlstring= file_get_contents($url, false, $context);
-//     $xml =simplexml_load_string($xmlstring, "SimpleXMLElement", LIBXML_NOCDATA);
-//     $json= json_encode($xml);
-//     $array= json_decode($json , TRUE);
 
-//     sendMessage($chatId,"Indique la palabra a buscar y le saldran 5 noticias que la contienen");
-//     // ForceReply('hola');
 
-// };
+function buscarNoticia($chatId,$palabra){
+    $context= stream_context_create(array('http'=> array('header'=>'Accept:application/xml')));
+    $url='https://www.europapress.es/rss/rss.aspx';
+    $xmlstring= file_get_contents($url, false, $context);
+    $xml =simplexml_load_string($xmlstring, "SimpleXMLElement", LIBXML_NOCDATA);
+    $json= json_encode($xml);
+    $array= json_decode($json , TRUE);
+
+    sendMessage($chatId,"Indique la palabra a buscar y le saldran 5 noticias que la contienen");
+    // ForceReply('hola');
+
+};
 ?>
