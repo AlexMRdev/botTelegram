db967ed38f6af1a58e4d4717427a9128119bec7a
AlexMRdev <alexmrdev@gmail.com>
2022-02-14 20:30:29 +0100

    2 dia

--- index.php
+++ index.php
@@ -17,9 +17,9 @@ switch($message) {
         $response = 'Hola! Soy @alex';
         sendMessage($chatId, $response);
         break;
-    // case '/noticia':
-    //     buscarNoticia($chatId);
-    //     break;
+    case '/categorias':
+        categorias($chatId);
+        break;
     case '/titulos':
         getPc($chatId);
         break;
@@ -44,6 +44,21 @@ function getPc($chatId){
     
     for($i=0; $i<=9; $i++ ){
         $titulos=$array['channel']['item'][$i]['title']."<a href='".$array['channel']['item'][$i]['link']."'>+info</a>"; 
+        sendMessage($chatId,$titulos);
+    };    
+};
+
+function categorias($chatId){
+    $context= stream_context_create(array('http'=> array('header'=>'Accept:application/xml')));
+    $url='https://www.europapress.es/rss/rss.aspx';
+    $xmlstring= file_get_contents($url, false, $context);
+    $xml =simplexml_load_string($xmlstring, "SimpleXMLElement", LIBXML_NOCDATA);
+    $json= json_encode($xml);
+    $array= json_decode($json , TRUE);
+    
+    for($i=0; $i<=9; $i++ ){
+        $titulos=$array['channel']['item'][$i]['category'];
+             
         sendMessage($chatId,$titulos);
     };    
 };
