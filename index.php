0f835817ae45c10288f60eca96207109bc6b7776
AlexMRdev <alexmrdev@gmail.com>
2022-02-15 20:38:20 +0100

    3 dia

--- index.php
+++ index.php
@@ -35,8 +35,9 @@ function sendMessage($chatId, $response,$repl) {
     if($repl == TRUE){
         $reply_mark=array('force_reply' => True);
         $url=$GLOBALS['website'].'/sendMessage?chat_id='.$chatId.'&parse_mode=HTML&reply_markup='.json_encode($reply_mark).'&text='.urlencode($response);
-    }else
+    }else{
     $url = $GLOBALS['website'].'/sendMessage?chat_id='.$chatId.'&parse_mode=HTML&text='.urlencode($response);
+    }
     file_get_contents($url);
 };
 
