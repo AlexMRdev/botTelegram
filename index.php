7812351b1c4e9a4072dba318b79faf5e355534ea
AlexMRdev <alexmrdev@gmail.com>
2022-02-17 16:37:36 +0100

    5 dia

--- index.php
+++ index.php
@@ -80,7 +80,7 @@ function titulos($chatId){
 
 function Mostrarcategorias($chatId,$reply){
     $context= stream_context_create(array('http'=> array('header'=>'Accept:application/xml')));
-    $url='https://www.elperiodico.com/es/rss/'.$reply.'/rss.xml';
+    $url='https://www.elperiodico.com/es/rss/deportes/rss.xml';
     $xmlstring= file_get_contents($url, false, $context);
     $xml =simplexml_load_string($xmlstring, "SimpleXMLElement", LIBXML_NOCDATA);
     $json= json_encode($xml);
